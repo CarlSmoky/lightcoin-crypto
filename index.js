015656e42e69b82a13d6b3fb293e0a46aaa8436c
tsumitakaoru <Katherine@KT-MacBook-Air-2.local>
2021-11-06 10:25:43 -0400

    add Deposit class

--- index.js
+++ index.js
@@ -12,12 +12,28 @@ class Withdrawal {
 
 }
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+// t1 = new Withdrawal(50.25);
+// t1.commit();
+// console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+// t2 = new Withdrawal(9.99);
+// t2.commit();
+// console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+// console.log('Balance:', balance);
+
+class Deposit {
+
+  constructor(amount) {
+    this.amount = amount;
+  }
+
+  commit() {
+    balance += this.amount;
+  }
+
+}
+
+t3 = new Deposit(120.00);
+t3.commit();
+console.log('Transaction 3:', t3);
